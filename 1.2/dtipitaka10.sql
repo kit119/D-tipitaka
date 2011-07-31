645aa338f21b86c9c7a8e98a319da08c5ec144ca
kit119 <kit119@gmail.com>
2011-07-31 10:33:59 +0700

    commit

---
+++ 1.2/dtipitaka10.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pali_siam (volume INT, page INT, items TEXT, content TEXT);
+CREATE TABLE thai_royal (volume INT, page INT, items TEXT, content TEXT);
+CREATE TABLE thai_mcu (volume INT, page INT, items TEXT, header TEXT, footer TEXT, display TEXT, content TEXT);
+CREATE TABLE thai_mbu (volume INT, volumn_orig INT, page INT, items TEXT, content TEXT);
+
---
+++ 1.2/dtipitaka12-fts4-icu48.sql
@@ -0,0 +1,4 @@
+CREATE VIRTUAL TABLE pali_siam USING fts4(volume INT, page INT, items TEXT, content TEXT, tokenize=icu th_TH);
+CREATE VIRTUAL TABLE thai_royal USING fts4(volume INT, page INT, items TEXT, content TEXT, tokenize=icu th_TH);
+CREATE VIRTUAL TABLE thai_mcu USING fts4(volume INT, page INT, items TEXT, header TEXT, footer TEXT, display TEXT, content TEXT, tokenize=icu th_TH);
+CREATE VIRTUAL TABLE thai_mbu USING fts4(volume INT, volume_orig INT, page INT, items TEXT, content TEXT, tokenize=icu th_TH);
